77f083cda548e4d9915d7c73b557651a05b87b2a
Ashutosh Chaudhari <164521610+Ashutosh-Chaudhari@users.noreply.github.com>
2026-09-16 02:50:19 +0530

    Time: 1 ms (99.93%), Space: 46.3 MB (81.6%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,32 +1,23 @@
-import java.util.HashSet;
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        HashSet<Character>[] rows = new HashSet[9];
-        HashSet<Character>[] cols = new HashSet[9];
-        HashSet<Character>[] boxes = new HashSet[9];
+        boolean[][] rows = new boolean[9][9];
+        boolean[][] cols = new boolean[9][9];
+        boolean[][] boxes = new boolean[9][9];
 
-        for(int i=0; i<9; i++){
-            rows[i] = new HashSet<>();
-            cols[i] = new HashSet<>();
-            boxes[i] = new HashSet<>();
-        }
-
-        for(int i=0; i<9; i++){
+        for(int i=0; i<9 ; i++){
             for(int j=0; j<9; j++){
                 char ch = board[i][j];
-                
-                if(ch=='.'){
+                if(ch == '.'){
                     continue;
                 }
-
+                int num = ch -'1';
                 int box = (i/3)*3 + (j/3);
-                if(rows[i].contains(ch) || cols[j].contains(ch) || boxes[box].contains(ch)){
+                if(rows[i][num] || cols[j][num] || boxes[box][num]){
                     return false;
                 }
-
-                rows[i].add(ch);
-                cols[j].add(ch);
-                boxes[box].add(ch);
+                rows[i][num] = true;
+                cols[j][num] = true;
+                boxes[box][num] = true;
             }
         }
         return true;
