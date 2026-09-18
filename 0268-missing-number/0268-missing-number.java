df3a60f075dc9868101f8dd7a8ba516e28626d5d
Ashutosh Chaudhari <164521610+Ashutosh-Chaudhari@users.noreply.github.com>
2026-09-18 20:57:47 +0530

    Time: 0 ms (100%), Space: 47.6 MB (13.92%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,12 +1,15 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        int n = nums.length;
-        Arrays.sort(nums);
-        for(int i=0; i<n; i++){
-            if(nums[i] != i){
+        int n =nums.length;
+        boolean[] seen = new boolean[n+1];
+        for(int num:nums){
+            seen[num] = true;
+        }
+        for(int i=0; i<=n; i++){
+            if(seen[i] == false){
                 return i;
             }
         }
-        return n;
+        return -1;
     }
 }
